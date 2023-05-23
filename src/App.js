c5be17f2d0ec383a0336784acdae07b149174957
Seungjun Oh <osjuny11@gmail.com>
2023-05-23 20:31:22 +0900

    format code

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          This is example page for seminar of CI/CD with <code>GitHub Actions</code>.
+          This is example page for seminar of CI/CD with{' '}
+          <code>GitHub Actions</code>.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
